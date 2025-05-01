173b2413178ca420c06978301bce26f8923d3faa
leprikon000 <160935098+leprikon000@users.noreply.github.com>
2025-05-01 20:41:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -112,5 +112,5 @@ app.post("/payment/callback", async (req, res) => {
   res.send("OK");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("🚀 Server running on port", PORT));
